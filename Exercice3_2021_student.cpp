37101f07c572113f76e80280f72f0c9fd3c0e5ae
johnthelennon <79632815+johnthelennon@users.noreply.github.com>
2021-11-19 15:41:57 +0100

    Add files via upload

--- Exercice3_2021_student.cpp
+++ Exercice3_2021_student.cpp
@@ -24,7 +24,7 @@ private:
     if((!force && last>=sampling) || (force && last!=1))
     {
       double emec = L*L*thetadot*thetadot*m*0.5 - m*g*L*cos(theta); // TODO: Evaluer l'energie mecanique
-      double pnc =  -kappa*L*L*thetadot*thetadot +m*L*thetadot*Omega*Omega*cos(Omega*t-theta) ; // TODO: Evaluer la puissance des forces non conservatives
+      double pnc =  -kappa*L*L*thetadot*thetadot +m*L*r*thetadot*Omega*Omega*cos(Omega*t-theta) ; // TODO: Evaluer la puissance des forces non conservatives
 
       *outputFile << t << " " << theta << " " << thetadot << " " << emec << " " << pnc << endl;
       last = 1;
@@ -42,16 +42,13 @@ private:
   }
 
   void step()
-  {double t_ini = t ;
-double theta_ini = theta;
-double a = acceleration(theta,thetadot,t);
-    theta = theta + thetadot*dt + 0.5*a*dt*dt;     // TODO: Implémentation du schéma de Verlet
-    double thetadot_1_2 = thetadot + 0.5*a*dt;
-    double a1= acceleration(theta_ini, 0, t_ini);
-    double a2=acceleration(theta,0,t+dt);
-    double a3=acceleration(0,thetadot_1_2,(1.0/Omega)*M_PI/2.0);
-    thetadot = thetadot + 0.5*dt*(a1 + a2) + a3*dt;
-    t+=dt;
+  {double temp0;
+      temp0=theta;
+      theta+= thetadot*dt+acceleration(theta,thetadot,t)*dt*dt*0.5;
+      double temp1;
+      temp1 = thetadot+acceleration(temp0,thetadot,t)*dt*0.5;
+      thetadot += (acceleration(temp0,temp1,t)+acceleration(theta,temp1,t+dt))*dt*0.5;
+      t+=dt;
   }
 
 
@@ -98,10 +95,10 @@ public:
     t = 0.;
     last = 0;
     printOut(true);
+    int i(0);
     while( t < tFin-0.5*dt )
     {
       step();
-      t += dt;
       printOut(false);
     }
     printOut(true);
